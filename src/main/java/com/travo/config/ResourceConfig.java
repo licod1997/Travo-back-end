b5fbcacabeaba3e0fa98212a0e272f54bdf55711
licod1997 <licod1997@gmail.com>
2018-03-24 23:28:05 +0900

    Adding Resources handler

--- src/main/java/com/travo/config/ResourceConfig.java
+++ src/main/java/com/travo/config/ResourceConfig.java
@@ -5,19 +5,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan
 public class ResourceConfig extends WebMvcConfigurerAdapter {
-	 private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-	          "classpath:/images/",
-	            "classpath:/static/", "classpath:/public/" };
 
-	    @Override
-	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	        if (!registry.hasMappingForPattern("/**")) {
-	            registry.addResourceHandler("/**").addResourceLocations(
-	                    CLASSPATH_RESOURCE_LOCATIONS);
-	        }
-	    }
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/images/",
+            "classpath:/static/", "classpath:/public/" };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/webjars/**")) {
+            registry.addResourceHandler("/webjars/**").addResourceLocations(
+                    "classpath:/META-INF/resources/webjars/");
+        }
+        if (!registry.hasMappingForPattern("/**")) {
+            registry.addResourceHandler("/**").addResourceLocations(
+                    CLASSPATH_RESOURCE_LOCATIONS);
+        }
+    }
 }
+
